940128d2f43f37deb5ce76974a847b2037a4ec45
tangpoo <lhinav@naver.com>
2024-02-05 20:06:27 +0900

    스웨거 디펜던시, 설정 추가

---
+++ src/main/java/com/sparta/todoapp/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.sparta.todoapp.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi(){
+        return GroupedOpenApi.builder()
+                .group("api")
+                .pathsToMatch("api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI(){
+        String title = "Todo Application Swagger";
+        String description = "Todo Application 의 Swagger 문서입니다.";
+
+        Info info = new Info().title(title).description(description).version("1.0.0");
+
+        return new OpenAPI().info(info);
+    }
+}
