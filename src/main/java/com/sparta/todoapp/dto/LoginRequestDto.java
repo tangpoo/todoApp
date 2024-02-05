ae2d12d3a223a757e0c78ff6b61e47b17a830288
tangpoo <lhinav@naver.com>
2024-02-05 14:39:54 +0900

    SignupRequestDto 조건식 추가

--- src/main/java/com/sparta/todoapp/dto/LoginRequestDto.java
+++ src/main/java/com/sparta/todoapp/dto/LoginRequestDto.java
@@ -1,5 +1,7 @@
 package com.sparta.todoapp.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/com/sparta/todoapp/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/todoapp/dto/SignupRequestDto.java
@@ -3,6 +3,8 @@ package com.sparta.todoapp.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,13 +13,18 @@ import lombok.Setter;
 @Setter
 public class SignupRequestDto {
     @NotBlank
+    @Size(min = 4, max = 10)
+    @Pattern(regexp = "^[a-z0-9]*$")
     private String username;
 
     @NotBlank
+    @Size(min = 8, max = 15)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String password;
 
     @Email
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9]*@[a-zA-Z0-9]*$")
     private String email;
 
     private boolean admin = false;
