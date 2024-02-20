6b22a0f43ce09180d422724f922aca94e47c783d
tangpoo <lhinav@naver.com>
2024-02-20 15:39:52 +0900

    임시로 test 를 위한 생성자 생성

--- src/main/java/com/sparta/todoapp/dto/schedule/ScheduleRequestDto.java
+++ src/main/java/com/sparta/todoapp/dto/schedule/ScheduleRequestDto.java
@@ -2,9 +2,11 @@ package com.sparta.todoapp.dto.schedule;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ScheduleRequestDto {
 
     @NotBlank
