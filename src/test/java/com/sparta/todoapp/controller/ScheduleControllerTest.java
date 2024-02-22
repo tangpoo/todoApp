bc9479564255a3fe06ccf51df45dfc22a7c6fe3c
tangpoo <lhinav@naver.com>
2024-02-23 00:53:06 +0900

    ScheduleController 테스트

--- src/test/java/com/sparta/todoapp/controller/ScheduleControllerTest.java
+++ src/test/java/com/sparta/todoapp/controller/ScheduleControllerTest.java
@@ -1,8 +1,9 @@
 package com.sparta.todoapp.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sparta.todoapp.common.CommonTest;
-import com.sparta.todoapp.common.TodoTest;
+import com.sparta.todoapp.common.ScheduleTest;
+import com.sparta.todoapp.dto.schedule.ScheduleListResponseDto;
 import com.sparta.todoapp.dto.schedule.ScheduleRequestDto;
 import com.sparta.todoapp.dto.schedule.ScheduleResponseDto;
 import com.sparta.todoapp.entity.Schedule;
@@ -12,26 +13,31 @@ import com.sparta.todoapp.service.ScheduleService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(ScheduleController.class)
-public class ScheduleControllerTest implements TodoTest {
+@WithMockUser
+public class ScheduleControllerTest implements ScheduleTest {
 
     @MockBean
     ScheduleService scheduleService;
@@ -55,8 +61,25 @@ public class ScheduleControllerTest implements TodoTest {
     }
 
     @Test
-    @DisplayName("스케줄 생성 요청")
-    @WithMockUser
+    @DisplayName("스케줄 생성 요청1")
+    void test0() throws Exception {
+        //given
+
+        //when
+        var action = mvc.perform(post("/api/schedule/new")
+                .with(csrf())
+                .header("Authorization", TEST_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(TEST_SCHEDULE_REQUEST_DTO)));
+
+        //then
+        action.andExpect(status().isOk());
+        verify(scheduleService, times(1)).createSchedule(eq(TEST_TOKEN), any(ScheduleRequestDto.builder().build().getClass()));
+    }
+
+    @Test
+    @DisplayName("스케줄 생성 요청2")
     void test1() throws Exception {
         //given
         ScheduleRequestDto requestDto = new ScheduleRequestDto("test_title", "test_content");
@@ -79,5 +102,43 @@ public class ScheduleControllerTest implements TodoTest {
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(jsonPath("$.message").value("스케줄 작성 성공"));
+    }
+
+    @Test
+    @DisplayName("스케줄 조회 요청")
+    void test2() throws Exception {
+        //given
+        given(scheduleService.getScheduleById(eq(TEST_TOKEN), eq(TEST_SCHEDULE_ID))).willReturn(TEST_SCHEDULE_RESPONSE_DTO);
+
+        //when
+        var action = mvc.perform(get("/api/schedule/{id}", TEST_SCHEDULE_ID)
+                .header("Authorization", TEST_TOKEN)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then
+        action
+                .andExpect(status().isOk());
+//                .andExpect(jsonPath("$.title").value(TEST_SCHEDULE_TITLE));
+//                .andExpect(jsonPath("$.content").value(TEST_SCHEDULE_CONTENT));
+    }
+
+    @Test
+    @DisplayName("스케줄 목록 조회 요청")
+    void test3() throws Exception {
+        //given
+        TEST_SCHEDULE_LIST.add(TEST_SCHEDULE_RESPONSE_DTO);
+        TEST_SCHEDULE_LIST.add(TEST_ANOTHER_SCHEDULE_RESPONSE_DTO);
+        ScheduleListResponseDto responseDto = new ScheduleListResponseDto(TEST_USER_NAME, TEST_SCHEDULE_LIST);
+
+        given(scheduleService.getSchedules(eq(TEST_TOKEN))).willReturn(responseDto);
+
+        //when
+        var action = mvc.perform(get("/api/schedule/schedules", TEST_TOKEN)
+                .header("Authorization", TEST_TOKEN)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then
+        action
+                .andExpect(status().isOk());
     }
 }
