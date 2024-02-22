a5e178902910804a06874fd6a96dbe6b65e18f3e
tangpoo <lhinav@naver.com>
2024-02-22 14:41:45 +0900

    UserService 테스트 구현

---
+++ src/test/java/com/sparta/todoapp/service/UserServiceTest.java
@@ -0,0 +1,208 @@
+package com.sparta.todoapp.service;
+
+import com.sparta.todoapp.dto.user.LoginRequestDto;
+import com.sparta.todoapp.dto.user.SignupRequestDto;
+import com.sparta.todoapp.dto.user.SignupResponseDto;
+import com.sparta.todoapp.entity.User;
+import com.sparta.todoapp.entity.UserRoleEnum;
+import com.sparta.todoapp.jwt.JwtUtil;
+import com.sparta.todoapp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @InjectMocks
+    UserService userService;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    PasswordEncoder passwordEncoder;
+
+    @Mock
+    JwtUtil jwtUtil;
+
+    @Test
+    @Transactional
+    @DisplayName("회원가입 실패, 유저 중복")
+    void test1(){
+        //given
+        SignupRequestDto requestDto = new SignupRequestDto();
+        requestDto.setUsername("test_username");
+        requestDto.setPassword("testPassword");
+        requestDto.setEmail("test@testEmail.com");
+
+        String username = requestDto.getUsername();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+        given(userRepository.findByUsername(username)).willReturn(Optional.of(new User()));
+
+        //when + then
+        assertThrows(DataIntegrityViolationException.class, () -> userService.signup(requestDto)) ;
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("회원가입 실패, email 중복")
+    void test2(){
+        //given
+        SignupRequestDto requestDto = new SignupRequestDto();
+        requestDto.setUsername("test_username");
+        requestDto.setPassword("testPassword");
+        requestDto.setEmail("test@testEmail.com");
+
+        String username = requestDto.getUsername();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+        given(userRepository.findByUsername(username)).willReturn(Optional.empty());
+        given(userRepository.findByEmail(requestDto.getEmail())).willReturn(Optional.of(new User()));
+
+        //when + then
+        assertThrows(DataIntegrityViolationException.class, () -> userService.signup(requestDto)) ;
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("회원가입 실패, 관리자 암호 실패")
+    void test3(){
+        //given
+        SignupRequestDto requestDto = new SignupRequestDto();
+        requestDto.setUsername("test_username");
+        requestDto.setPassword("testPassword");
+        requestDto.setEmail("test@testEmail.com");
+        requestDto.setAdmin(true);
+        requestDto.setAdminToken("test_fail_admin_token");
+
+        String username = requestDto.getUsername();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+        given(userRepository.findByUsername(username)).willReturn(Optional.empty());
+        given(userRepository.findByEmail(requestDto.getEmail())).willReturn(Optional.empty());
+
+        //when + then
+        assertThrows(AccessDeniedException.class, () -> userService.signup(requestDto));
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("회원가입 성공")
+    void test4(){
+        //given
+        SignupRequestDto requestDto = new SignupRequestDto();
+        requestDto.setUsername("test_username");
+        requestDto.setPassword("testPassword");
+        requestDto.setEmail("test@testEmail.com");
+        requestDto.setAdmin(true);
+        requestDto.setAdminToken("AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC");
+
+        String username = requestDto.getUsername();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+        User user = User.builder()
+                .username(username)
+                .email("test_email")
+                .password("test_password")
+                .role(UserRoleEnum.USER)
+                .build();
+
+        given(userRepository.findByUsername(username)).willReturn(Optional.empty());
+        given(userRepository.findByEmail(requestDto.getEmail())).willReturn(Optional.empty());
+        given(userRepository.save(any(User.class))).willReturn(user);
+
+        //when
+        SignupResponseDto responseDto = userService.signup(requestDto);
+
+        //then
+        assertEquals(requestDto.getUsername(), responseDto.getUserName());
+        assertEquals(user.getUsername(), responseDto.getUserName());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("로그인 회원 찾기 실패")
+    void test5() {
+        //given
+        LoginRequestDto requestDto = new LoginRequestDto();
+        requestDto.setUsername("test_name");
+        requestDto.setPassword("test_password");
+
+        User user = User.builder()
+                .username(requestDto.getUsername())
+                .email("test_email")
+                .password(requestDto.getPassword())
+                .role(UserRoleEnum.USER)
+                .build();
+
+        given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.empty());
+
+        //when + then
+        assertThrows(NoSuchElementException.class, () -> userService.login(requestDto));
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("로그인 비밀번호 틀림")
+    void test6() {
+        //given
+        LoginRequestDto requestDto = new LoginRequestDto();
+        requestDto.setUsername("test_name");
+        requestDto.setPassword("test_password");
+
+        User user = User.builder()
+                .username(requestDto.getUsername())
+                .email("test_email")
+                .password("test_diff_password")
+                .role(UserRoleEnum.USER)
+                .build();
+
+        given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.of(user));
+
+        //when + then
+        assertThrows(AccessDeniedException.class, () -> userService.login(requestDto));
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("로그인 성공")
+    void test7() {
+        //given
+        LoginRequestDto requestDto = new LoginRequestDto();
+        requestDto.setUsername("test_name");
+        requestDto.setPassword("test_password");
+
+        User user = User.builder()
+                .username(requestDto.getUsername())
+                .email("test_email")
+                .password(passwordEncoder.encode(requestDto.getPassword()))
+                .role(UserRoleEnum.USER)
+                .build();
+
+        given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.of(user));
+        given(passwordEncoder.matches(requestDto.getPassword(), user.getPassword())).willReturn(true);
+        //when
+        String token = userService.login(requestDto);
+
+        //then
+
+    }
+}
