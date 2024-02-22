ce0e2949c618a14322925793ec25f29d22961a13
tangpoo <lhinav@naver.com>
2024-02-22 12:29:36 +0900

    setter 추가

--- src/main/java/com/sparta/todoapp/dto/user/LoginRequestDto.java
+++ src/main/java/com/sparta/todoapp/dto/user/LoginRequestDto.java
@@ -2,8 +2,10 @@ package com.sparta.todoapp.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class LoginRequestDto {
     @NotBlank
     private String username;
